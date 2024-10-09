30c6f2a193880769c125b43b128f93f4c47686e0
lovrozagar <lovro.zagar5@gmail.com>
2024-10-09 15:19:53 -0700

    style: navbar

--- src/module/navbar/component/navbar-item.tsx
+++ src/module/navbar/component/navbar-item.tsx
@@ -1,20 +1,23 @@
 'use client'
 
-import { Button } from '@renderui/core'
+import { Button, cx } from '@renderui/core'
 import Link from 'next/link'
 import type { ComponentPropsWithRef } from 'react'
 
 type NavbarItemProps = ComponentPropsWithRef<typeof Link>
 
 const NavbarItem = (props: NavbarItemProps) => {
-	const { children, ...restProps } = props
+	const { children, className, ...restProps } = props
 
 	return (
 		<li>
 			<Button
 				asChild
 				variant='plain'
-				className='rounded-full py-1 px-4 data-[focus-visible=true]:ring-offset-[0px]'
+				className={cx(
+					'rounded-full py-1 px-4 text-neutral-400 font-semibold transition-colors data-[hover=true]:text-mode-contrast data-[focus-visible=true]:ring-offset-[0px]',
+					className,
+				)}
 			>
 				{({ Ripple }) => (
 					<Link {...restProps}>
--- src/module/navbar/component/navbar.tsx
+++ src/module/navbar/component/navbar.tsx
@@ -1,5 +1,6 @@
 import { ModeSwitch } from '@/component/mode-switch/mode-switch'
 import { NavbarItem } from '@/module/navbar/component/navbar-item'
+import { GitHubLogoIcon } from '@radix-ui/react-icons'
 import { Container } from '@renderui/core'
 
 function Navbar() {
@@ -7,10 +8,19 @@ function Navbar() {
 		<div className='border-b border-mode-accent'>
 			<Container>
 				<nav className='h-[50px] backdrop-blur-[10px] flex items-center w-full'>
-					<ul className='flex gap-x-8 items-center w-full'>
-						<NavbarItem href='/'>Home</NavbarItem>
-						<NavbarItem href='/'>Experience</NavbarItem>
-						<NavbarItem href='/'>Projects</NavbarItem>
+					<ul className='flex gap-x-3 items-center w-full'>
+						<NavbarItem
+							href='https://github.com/lovrozagar'
+							target='_blank'
+							referrerPolicy='no-referrer'
+							className='text-mode-contrast'
+						>
+							<GitHubLogoIcon />
+							lovrozagar
+						</NavbarItem>
+						<NavbarItem href='/'>home</NavbarItem>
+						<NavbarItem href='/'>experience</NavbarItem>
+						<NavbarItem href='/'>projects</NavbarItem>
 						<li className='ml-auto'>
 							<ModeSwitch />
 						</li>
