911238fc92db720961effa90bb99c74edb726f98
lovrozagar <lovro.zagar5@gmail.com>
2024-10-15 14:35:41 -0700

    feat: add carrer item type and data

---
+++ src/module/terminal/type/type.ts
@@ -0,0 +1,13 @@
+type CareerItem = {
+	organizationName: string
+	organizationHref: string | undefined
+	startDate: string
+	endDate: string
+	role: string
+	commitmentType: string
+	location: string
+	skills: string[]
+	description: string[]
+}
+
+export type { CareerItem }
